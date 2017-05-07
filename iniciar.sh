651dc0660aea7d9c825dcfa3ae626f7114c785e0
Rocío Márquez <arixha@hotmail.com>
2017-05-07 20:22:51 +0200

    limpiar.sh y iniciar.sh ok

--- iniciar.sh
+++ iniciar.sh
@@ -2,9 +2,10 @@
 #@Author Vitor Carvalho
 
 # pedro.herruzo@ub.edu
+
 chmod u+x limpiar.sh
 ./limpiar.sh
-rm *.pid #elimina los ficheros pid ya existentes
+
 make #inicia los ficheros c por medio del fichero makefile
 ./minutos &
 ./segundos &
